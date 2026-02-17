be5abe17b3776dcd993ae3f14db5513c573f5416
sudharsandhoni <sudharsandhonisalem@gmail.com>
2026-02-17 08:09:04 +0530

    Removed unnecessary package-info.java files

--- src/test/java/tests/SampleTest.java
+++ src/test/java/tests/SampleTest.java
@@ -1,13 +1,11 @@
 package tests;
-import static core.utils.LogUtils.*;
-import org.testng.annotations.*;
 
 public class SampleTest extends BaseTest{
 	
-	@Test
-	public void Test1() {
-		System.out.println("Smoke Test");
-		logInfo("Running test1");
-	}
+//	@Test
+//	public void Test1() {
+//		System.out.println("Smoke Test");
+//		logInfo("Running test1");
+//	}
 
 }
